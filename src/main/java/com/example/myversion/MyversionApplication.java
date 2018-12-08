8f9ec7000989cf90152b49ec0138bc6652834ec2
Simon <simo340c@stud.kea.dk>
2018-12-08 20:38:10 +0100

    first commit

---
+++ src/main/java/com/example/myversion/MyversionApplication.java
@@ -0,0 +1,12 @@
+package com.example.myversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyversionApplication.class, args);
+    }
+}
